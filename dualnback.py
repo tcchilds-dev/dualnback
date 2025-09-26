dcbe0665d887737501770aad6189c56984b4500a
Thomas Childs <thomas.c.childs@gmail.com>
2025-09-26 17:41:59 +0100

    Clean up the codebase with Grid

--- dualnback.py
+++ dualnback.py
@@ -4,25 +4,38 @@ import random
 from pygame.math import Vector2
 import sys
 
-class GRID:
+class Grid:
+    grid_top_left = pygame.Surface((140,140))
+    grid_top_left.fill(pygame.Color("white"))
+
+    grid_top_center = pygame.Surface((140,140))
+    grid_top_center.fill(pygame.Color("white"))
+
+    grid_top_right = pygame.Surface((140,140))
+    grid_top_right.fill(pygame.Color("white"))
+
+    grid_mid_left = pygame.Surface((140,140))
+    grid_mid_left.fill(pygame.Color("white"))
+
+    grid_mid_center = pygame.Surface((140,140))
+    grid_mid_center.fill(pygame.Color("white"))
+
+    grid_mid_right = pygame.Surface((140,140))
+    grid_mid_right.fill(pygame.Color("white"))
+
+    grid_bot_left = pygame.Surface((140,140))
+    grid_bot_left.fill(pygame.Color("white"))
+
+    grid_bot_center = pygame.Surface((140,140))
+    grid_bot_center.fill(pygame.Color("white"))
+
+    grid_bot_right = pygame.Surface((140,140))
+    grid_bot_right.fill(pygame.Color("white"))
+
+class GameButton:
     ...
 
-class POSBTN:
-    ...
-
-class SOUNDBTN:
-    ...
-
-class LVLDISPLAY:
-    ...
-
-class STARTBTN:
-    ...
-
-class RESULTTBLE:
-    ...
-
-class MAIN:
+class Main:
     ...
 
 pygame.init()
@@ -31,36 +44,10 @@ DISPLAYSURF = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
 game_font = pygame.font.Font(None, 80)
 game_active = True
+grid = Grid()
 
 """ SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 100) """
-
-grid_top_left = pygame.Surface((140,140))
-grid_top_left.fill(pygame.Color("white"))
-
-grid_top_center = pygame.Surface((140,140))
-grid_top_center.fill(pygame.Color("white"))
-
-grid_top_right = pygame.Surface((140,140))
-grid_top_right.fill(pygame.Color("white"))
-
-grid_mid_left = pygame.Surface((140,140))
-grid_mid_left.fill(pygame.Color("white"))
-
-grid_mid_center = pygame.Surface((140,140))
-grid_mid_center.fill(pygame.Color("white"))
-
-grid_mid_right = pygame.Surface((140,140))
-grid_mid_right.fill(pygame.Color("white"))
-
-grid_bot_left = pygame.Surface((140,140))
-grid_bot_left.fill(pygame.Color("white"))
-
-grid_bot_center = pygame.Surface((140,140))
-grid_bot_center.fill(pygame.Color("white"))
-
-grid_bot_right = pygame.Surface((140,140))
-grid_bot_right.fill(pygame.Color("white"))
 
 level_surface = game_font.render("n = 1", True, (255,255,255))
 level_surface_rect = level_surface.get_rect(center = (400,50))
@@ -75,7 +62,7 @@ sound_btn.fill(pygame.Color("white"))
 sound_text = game_font.render("SOUND", True, (0,0,0))
 sound_text_rect = sound_text.get_rect(center = (600,675))
 
-main_game = MAIN()
+main_game = Main()
 
 # Game loop
 while True:
@@ -91,15 +78,15 @@ while True:
             game_active = True
     
     if game_active: # playing a level
-        DISPLAYSURF.blit(grid_top_left, (170,80))
-        DISPLAYSURF.blit(grid_top_center, (330,80))
-        DISPLAYSURF.blit(grid_top_right, (490,80))
-        DISPLAYSURF.blit(grid_mid_left, (170,240))
-        DISPLAYSURF.blit(grid_mid_center, (330,240))
-        DISPLAYSURF.blit(grid_mid_right, (490,240))
-        DISPLAYSURF.blit(grid_bot_left, (170,400))
-        DISPLAYSURF.blit(grid_bot_center, (330,400))
-        DISPLAYSURF.blit(grid_bot_right, (490,400))
+        DISPLAYSURF.blit(grid.grid_top_left, (170,80))
+        DISPLAYSURF.blit(grid.grid_top_center, (330,80))
+        DISPLAYSURF.blit(grid.grid_top_right, (490,80))
+        DISPLAYSURF.blit(grid.grid_mid_left, (170,240))
+        DISPLAYSURF.blit(grid.grid_mid_center, (330,240))
+        DISPLAYSURF.blit(grid.grid_mid_right, (490,240))
+        DISPLAYSURF.blit(grid.grid_bot_left, (170,400))
+        DISPLAYSURF.blit(grid.grid_bot_center, (330,400))
+        DISPLAYSURF.blit(grid.grid_bot_right, (490,400))
 
         DISPLAYSURF.blit(level_surface, level_surface_rect)
 
